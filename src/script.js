681d2c5ee3d05ca408629336a3e647b5f8241b1d
Diego-Zerwes <difoz2003@hotmail.com>
2024-06-28 19:42:28 -0300

    terminado

--- src/script.js
+++ src/script.js
@@ -1,4 +1,18 @@
-function formulario() {
+
+let data = [];
+function transformToHtml(arrayDeUsers) {
+  const usersHtml = arrayDeUsers.map((user, index) => {
+    return card(user) // user.Nome; user.cpf; user.dataNascimento
+  }).join("");
+
+  console.log(usersHtml)
+
+  return usersHtml;
+}
+
+function formulario(arrayDeUsers) {
+
+
   return ` <form id="myForm"class="max-w-sm mx-auto">
     <div class="mb-5">
       <label for="large-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome</label>
@@ -19,13 +33,32 @@ function formulario() {
 `;
 }
 
+async function updateUI() {
+  data = await carregarPessoas();
+  const resultado = document.getElementById("result");
+
+  console.log(data);
+
+  if (data) {
+    resultado.innerHTML = transformToHtml(data);
+  }
+}
+
+(async () => {
+  await updateUI();
+  await botaoDelete()
+})();
+
 document.addEventListener("DOMContentLoaded", function () {
   const container = document.getElementById("container");
 
   container.innerHTML = formulario();
 
   const submitButton = document.getElementById("submitButton");
-  submitButton.addEventListener("click", apiForms);
+  submitButton.addEventListener("click", async () => {await apiForms()
+window.location.reload(true);
+  
+  });
 });
 
 async function apiForms() {
@@ -36,7 +69,7 @@ async function apiForms() {
     const cpf = document.getElementById("small-input-cpf").value;
 
     console.log(nome, dataNascimento, cpf);
-    const data = {
+    const formData = {
       nome,
       dataNascimento,
       cpf,
@@ -49,9 +82,10 @@ async function apiForms() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify(formData),
       }
     );
+    console.log(formData)
     console.log(response);
     if (response.ok) {
       const pessoa = await response.json();
@@ -65,23 +99,86 @@ async function apiForms() {
   }
 }
 
-function imprimir(pessoa) {
-  const resultado = document.getElementById("result");
+function card(user){
+return`
+  <div class="card" data-id="${user.Id}">
+  <h3 class="text-lg font-medium text-gray-900 dark:text-white">Pessoa Adicionada</h3>
+  <p><strong>Nome:</strong> ${user.Nome || "Nome n existe"}</p>
+  <p><strong>Data de Nascimento:</strong> ${user.DataNascimento || "nao existe"}</p>
+  <p><strong>CPF:</strong> ${user.CPF || "nao existe"}</p>
+  <button class='btn-delete w-50 h-50 px-4 py-2 bg-red-600 text-white'>Delete</button>
+  </div>
+`;
+}
 
-  // Verifica se a resposta contém os dados esperados
+
+
+function imprimir(pessoa) {
+
   if (pessoa && pessoa.nome && pessoa.dataNascimento && pessoa.cpf) {
     resultado.innerHTML = `<h3 class="text-lg font-medium text-gray-900 dark:text-white">Pessoa Adicionada</h3>
-      <p><strong>Nome:</strong> ${pessoa.nome}</p>
+      <p><strong>Nome:</strong> ${pessoa.nome || "Nome n existe"}</p>
       <p><strong>Data de Nascimento:</strong> ${pessoa.dataNascimento}</p>
       <p><strong>CPF:</strong> ${pessoa.cpf}</p>`;
   } else {
-    exibirErro(); // Exibe mensagem de erro na página
+    
+    exibirErro(); 
+    
   }
 }
 
-// Função para exibir mensagem de erro na página
 function exibirErro() {
   const resultado = document.getElementById("result");
   resultado.innerHTML = `<h3 class="text-lg font-medium text-gray-900 dark:text-white">Erro ao receber dados</h3>
     <p>Os dados da pessoa não foram recebidos corretamente.</p>`;
+}
+
+ 
+
+async function carregarPessoas(){
+  const url = await fetch("https://personal-tp6a9zfc.outsystemscloud.com/Pessoas/rest/Pessoas/Pessoas");
+  
+  if(!url.ok){
+    throw new error('erro na aquisição:' + url.status)
+  }
+
+  const pessoas = await url.json();
+
+  console.log(pessoas);
+  return pessoas;
+}
+
+async function deletePessoa(Id){
+  try{
+    const resposta = await fetch(`https://personal-tp6a9zfc.outsystemscloud.com/Pessoas/rest/Pessoas/DeletePessoaById?PessoaId=${Id}`,{
+      
+      method: "DELETE"
+
+    });
+
+    if(!resposta.ok){
+      throw new Error('houve um erro')
+    }
+
+    // const dadosResposta = resposta.json();
+    // console.log(dadosResposta)
+
+  }catch(error){
+    console.log(error)
+  }
+}
+
+async function botaoDelete(){
+  const botoes = document.querySelectorAll('.btn-delete')
+
+  botoes.forEach(botao => {
+    botao.addEventListener('click', async function(e){
+      const id = e.target.parentElement.dataset.id;
+      await deletePessoa(id).then(async () => {
+        await updateUI();
+        window.location.reload(true);
+
+      });
+    })
+  })
 }
